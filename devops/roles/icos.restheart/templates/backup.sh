c581a7ad27172e1bccf2a418ab375fd99ff6e869
André Bjärby <andre.bjarby@nateko.lu.se>
2021-11-07 00:04:18 +0100

    restheart - make bbclient import part of restheart role

--- devops/roles/icos.restheart/templates/backup.sh
+++ devops/roles/icos.restheart/templates/backup.sh
@@ -8,7 +8,7 @@ mkdir -p backup
 
 docker-compose exec -T mongodb mongodump --quiet --archive > backup/server.archive
 
-bin/bbclient-all create '::{now}' backup
-bin/bbclient-all prune --keep-within 7d --keep-daily=30 --keep-weekly=150
+{{ bbclient_all }} create '::{now}' backup
+{{ bbclient_all }} prune --keep-within 7d --keep-daily=30 --keep-weekly=150
 
 rm -rf -- ./backup
