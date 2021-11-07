cd62be842e935395db56e5fc6718cec583922601
André Bjärby <andre.bjarby@nateko.lu.se>
2021-11-07 19:09:17 +0100

    quince - integrated bbclient into the role

--- devops/roles/icos.quince/templates/quince-backup.sh
+++ devops/roles/icos.quince/templates/quince-backup.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-# Backup
-
-set -e
-set -u
-
-PATH=$HOME/bin:$PATH
+set -Eueo pipefail
 
 tmp=$(mktemp -d /tmp/quince-backup.XXXXXX)
 trap 'rm -rf "$tmp"' EXIT
@@ -14,6 +9,6 @@ cd "$tmp"
 mysqldump --single-transaction --quick --user=quince --password=quince --hex-blob quince 2>&1 > mysql.dump \
     | awk '! /Using a password on the command line interface can be insecure./'
 
-bbclient-all create '::{now}' mysql.dump /opt/quince_filestore
+{{ bbclient_all }} create '::{now}' mysql.dump /opt/quince_filestore
 
-bbclient-all prune --keep-within 7d --keep-daily=30 --keep-weekly=150
+{{ bbclient_all }} prune --keep-within 7d --keep-daily=30 --keep-weekly=150
