ba8b609e49731b5d710eb8c01fbf6e388f7b9685
André Bjärby <andre.bjarby@nateko.lu.se>
2021-11-16 11:08:07 +0100

    rdflogdb - remove old directory

--- rdflogdb/master/build/example.init-db.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-	CREATE USER rep REPLICATION
-	LOGIN CONNECTION LIMIT 1
-	PASSWORD 'some_replication_password';
-EOSQL
