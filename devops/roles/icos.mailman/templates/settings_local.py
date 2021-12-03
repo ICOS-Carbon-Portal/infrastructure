884998f698129b66d1f8c42b96982c2c54cd219a
André Bjärby <andre.bjarby@nateko.lu.se>
2021-12-03 08:00:44 +0100

    mailman - refresh
    + Dump versions of docker images
    + Simplify role
    + Disable social authentication
    + Add cli wrappers

--- devops/roles/icos.mailman/templates/settings_local.py
+++ devops/roles/icos.mailman/templates/settings_local.py
@@ -1,4 +1,4 @@
-# see https://github.com/maxking/docker-mailman/blob/master/web/mailman-web/settings.py for examples of how this file should look
+# https://github.com/maxking/docker-mailman/blob/master/web/mailman-web/settings.py
 
 ALLOWED_HOSTS = [
     "localhost",
@@ -9,5 +9,11 @@ ALLOWED_HOSTS = [
     {% endfor %}
 ]
 
+{% if mailman_rest_pass is defined -%}
 MAILMAN_REST_API_USER = "{{ mailman_rest_user }}"
 MAILMAN_REST_API_PASS = "{{ mailman_rest_pass }}"
+{% endif -%}
+
+# https://github.com/maxking/docker-mailman/tree/main/web#disable-social-auth
+# This will remove all "login with google" etc buttons the web page.
+MAILMAN_WEB_SOCIAL_AUTH = []
