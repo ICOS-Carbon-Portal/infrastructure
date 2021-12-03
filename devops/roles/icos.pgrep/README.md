b180c53fb9448794bea67db82a123c88542f927a
André Bjärby <andre.bjarby@nateko.lu.se>
2021-12-03 10:55:07 +0100

    pgrep - update role
    + No longer bulid an image but use default postgres image.
    + Mount the init script in docker-compose instead.
    + Add a postgres prometheus exporter
    + Simplify tasks

---
+++ devops/roles/icos.pgrep/README.md
@@ -0,0 +1,7 @@
+## To test the prometheus_metrics container
+
+    wget -qO- localhost:9187/metrics
+
+That will connect to the exposed port and retrive metrics. It's not until one
+request metrics that the exporter will try to connect to postgres (and thus
+trigger any connection errors).
--- devops/roles/icos.pgrep/templates/entrypoint.sh
+++ devops/roles/icos.pgrep/templates/entrypoint.sh
@@ -2,16 +2,24 @@
 
 set -Eeo pipefail
 
+if [ "$(id -u)" = '0' ]; then
+    chown -R postgres:postgres "$PGDATA/.."
+    chmod 700 "$PGDATA"
+    chmod 600 "$PGDATA/../.pgpass"
+    exec gosu postgres "$BASH_SOURCE" "$@"
+fi
+
+# Check whether database has been initialized.
 if [ ! -s "$PGDATA/PG_VERSION" ]; then
-	echo "Connecting to peer and making sure that our replication slot exists"
-	psql -d "$CONNINFO" -c "select * from \
-	                        pg_create_physical_replication_slot('$SLOTNAME') \
-							where not exists (\
-							select 1 from pg_replication_slots \
-							where slot_name = '$SLOTNAME')"
-	echo "Starting basebackup"
-	pg_basebackup -D "$PGDATA" -R -v -S "$SLOTNAME" -d "$CONNINFO" -w
-	echo "Basebackup finished"
+    echo "Connecting to peer and making sure that our replication slot exists"
+    psql -d "$CONNINFO" -c "select * from                                              \
+                            pg_create_physical_replication_slot('$SLOTNAME')           \
+                                                    where not exists (                 \
+                                                    select 1 from pg_replication_slots \
+                                                    where slot_name = '$SLOTNAME')"
+    echo "Starting basebackup"
+    pg_basebackup -D "$PGDATA" -R -v -S "$SLOTNAME" -d "$CONNINFO" -w
+    echo "Basebackup finished"
 fi
 
 echo "Starting postgres server"
