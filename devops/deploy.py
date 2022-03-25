929fb92ac0aae217e224dc4e273004b9a3e73fb5
André Bjärby <andre.bjarby@nateko.lu.se>
2022-03-25 09:01:28 +0100

    update deploy.py
    Update docs.
    Add special case for showing facts.

--- devops/deploy.py
+++ devops/deploy.py
@@ -10,13 +10,16 @@
 # Installation:
 # $ ln -s deploy.py $HOME/bin/deploy
 #
+# Special case, show facts for host:
+# $ deploy facts fsicos2
+#
 # Show tags for cpmeta:
 # $ deploy cpmeta --list-tasks
 #
 # Show tasks for cpmeta:
 # $ deploy cpmeta --list-tasks
 #
-# Run only the backup tasks.
+# Run only the backup tasks in the cpmeta playbook.
 # $ deploy cpmeta backup
 #
 # Deploy new version of cpmeta to production.
@@ -47,15 +50,20 @@ if not os.path.exists(INVENTORY):
     die("Cannot find %s in %s" % (INVENTORY, DEVOPS))
 
 NAME = sys.argv.pop(0)
-PLAYBOOK = "%s.yml" % NAME
-if not os.path.exists(PLAYBOOK):
-    die("Cannot find %s in %s" % (PLAYBOOK, DEVOPS))
 
-# The remaining arguments are either tags or options to ansible.
-TAGS = ["-t%s" % tag for tag in sys.argv if not tag.startswith('-')]
-OPTS = [opt for opt in sys.argv if opt.startswith('-')]
-ARGS = ['ansible-playbook', '-i', INVENTORY, PLAYBOOK] + TAGS + OPTS
+# Special case - print facts for host.
+if len(sys.argv) == 1 and NAME == "facts":
+    ARGS = ["ansible", sys.argv[0], '-m', 'setup', '-i', INVENTORY]
+# Standard case - run playbook.
+else:
+    PLAYBOOK = "%s.yml" % NAME
+    if not os.path.exists(PLAYBOOK):
+        die("Cannot find %s in %s" % (PLAYBOOK, DEVOPS))
 
-print("ansible-playbook", *ARGS)
+    # The remaining arguments are either tags or options to ansible.
+    TAGS = ["-t%s" % tag for tag in sys.argv if not tag.startswith('-')]
+    OPTS = [opt for opt in sys.argv if opt.startswith('-')]
+    ARGS = ['ansible-playbook', '-i', INVENTORY, PLAYBOOK] + TAGS + OPTS
 
-os.execvp('ansible-playbook', ARGS)
+print(*ARGS)
+os.execvp(ARGS[0], ARGS)
