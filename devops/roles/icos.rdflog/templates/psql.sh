485eaa3ebf1fc84e882909dcd04be7b7a5c5f780
André Bjärby <andre.bjarby@nateko.lu.se>
2022-06-02 07:46:29 +0200

    rdflog - refactor role
    Don't build postgres image but use the standard one directly.
    Remove the ctl.sh script.
    Adapt psql.sh to use docker-compose.
    Simplify.

--- devops/roles/icos.rdflog/templates/psql.sh
+++ devops/roles/icos.rdflog/templates/psql.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-set -e
+set -Eueo pipefail
 
 cd "{{ rdflog_home }}"
 
-# If stdout is a tty then we need the '-t' flag, otherwise we're redirecting to
-# a file and we must not have "-t". 
-if [ -t 0 ] && [ -t 1 ]; then O="-it"; else O="-i"; fi
+if [ -t 0 ] && [ -t 1 ]; then T=""; else T="-T"; fi
 
-# And just when you thought it was cloudy enough!
-# https://github.com/docker/compose/issues/3352
-CID=$(docker-compose -f docker-compose.yml ps -q db)
-
-docker exec "$O" "$CID" psql "$@"
+docker-compose exec $T -u postgres db psql "$@"
