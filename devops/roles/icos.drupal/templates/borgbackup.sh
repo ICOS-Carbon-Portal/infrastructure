bf6924b5263acd85c5a3c038ecaa3555496613d2
André Bjärby <andre.bjarby@nateko.lu.se>
2022-12-31 12:05:21 +0100

    drupal - use systemd timer for backups

--- devops/roles/icos.drupal/templates/borgbackup.sh
+++ devops/roles/icos.drupal/templates/borgbackup.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-set -Eeuo pipefail
-LOGFILE=backup.log
+set -xEeuo pipefail
 
 for project in {{ drupal_websites | join(" ") }}; do
     cd "{{ drupal_home }}"
 
     if [ ! -d "$project" ]; then
-        echo "$project directory not found. Skipping." >> "$LOGFILE"
+        echo "$project directory not found. Skipping."
         continue
     fi
 
     cd "$project/drupal/docker"
 
-    docker-compose down >& /dev/null
+    docker-compose down
+
     # If bbclient fails, it might be because one of its repos cannot be
     # reached. In that case we want to continue looping through the other
     # projects
-    {{ bbclient_all }} create --verbose --stats "::$project-{now}" {{ drupal_home }}/$project/drupal >> "$LOGFILE" 2>&1 || :
-    docker-compose up -d >& /dev/null
+    {{ bbclient_all }} create -xvs "::$project-{now}" {{ drupal_home }}/$project/drupal || :
 
-    # prune backups
-    {{ bbclient_all }} prune --keep-daily=90 --keep-weekly=-1 --prefix="$project-"
+    docker-compose up -d
+
+    {{ bbclient_all }} prune -vs --keep-daily=90 --keep-weekly=-1 --prefix="$project-"
 done
