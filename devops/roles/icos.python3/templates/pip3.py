36c5779d57cf7c6d68e0d1d4adf862896ac2d9f4
André Bjärby <andre.bjarby@nateko.lu.se>
2023-03-10 12:45:22 +0100

    python3 - simplify

--- devops/roles/icos.python3/templates/pip3.py
+++
@@ -1,9 +0,0 @@
-#!{{ python3_command }}
-# -*- coding: utf-8 -*-
-# This is a copy of /usr/local/bin/pip3.
-import re
-import sys
-from pip._internal.cli.main import main
-if __name__ == '__main__':
-    sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
-    sys.exit(main())
