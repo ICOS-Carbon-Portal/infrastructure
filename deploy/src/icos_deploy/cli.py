6096ad8368cc348572154a30d51e276b89d4981f
André Bjärby <andre.bjarby@nateko.lu.se>
2023-04-18 08:18:12 +0200

    move deploy script to its own directory
    Also structure it like a python project

--- deploy/src/icos_deploy/cli.py
+++ deploy/src/icos_deploy/cli.py
@@ -4,26 +4,41 @@ import sys
 import os
 import click
 import glob
+from pathlib import Path
 
 
 # Instruct click to pass unknown options and extra arguments unmolested. This
-# way we pass them on to ansible.
+# way we can pass them on to ansible.
 PASS_ARGS = dict(context_settings=dict(
     ignore_unknown_options=True,
     allow_interspersed_args=False,
     allow_extra_args=True,
 ))
 
+# Most subcommands take the same inventory parameter, define it here.
+OPTION_INVENTORY = click.option("-i", "--inventory",
+                                default="production",
+                                show_default=True,
+                                help='select inventory')
 
+
+# UTILS
 def die(msg):
     print(msg, file=sys.stderr)
     sys.exit(1)
 
 
 def find_playbook(expr):
+    """Use a glob-like expression to find playbooks.
+
+    Examples:
+    "foo.yml" matches ./foo.yml
+    "foo" also matches ./foo.yml
+    "prom/vm" matches ./prometheus/vmagent.yml
+    """
     # first try an exact match
-    playbook = f'{expr}.yml'
-    if os.path.exists(playbook):
+    playbook = Path(f'{expr}.yml')
+    if playbook.exists():
         return playbook
 
     # maybe the user wants a subdirectory
@@ -38,29 +53,50 @@ def find_playbook(expr):
     elif len(matches) > 1:
         die(f'More than one playbook match "{expr}" - {",".join(matches)}')
 
-    return matches[0]
+    return Path(matches[0])
 
 
+def find_devops_dir():
+    """Find the devops directory, starting from __file__.
+    """
+    # __file__ == X/infrastructure/deploy/src/icos_deploy/cli.py
+    here = Path(__file__)
+    devops = here.parent.parent.parent.parent / 'devops'
+    assert devops.is_dir()
+    return devops
 
+
+# CLI
 @click.group()
 def cli():
-    pass
+    # Change to the directory where the playbooks lives. Ansible uses the
+    # current working directory as a starting point when locating its files.
+    devops = find_devops_dir()
+    print(f'cd {devops}')
+    os.chdir(devops)
 
 
 @cli.command(**PASS_ARGS)
-@click.argument("host")
-@click.option("-i", "inventory", default="production")
 @click.pass_context
-def facts(ctx, host, inventory):
+@OPTION_INVENTORY
+@click.argument("host")
+def facts(ctx, inventory, host):
+    """Dump all gathered facts for host by running the setup module.
+    """
     cmd = ["ansible", host,"-m", "setup", f"-i{inventory}.inventory"]
     print(*cmd, *ctx.args)
     os.execvp(cmd[0], cmd + ctx.args)
 
 
 @cli.command()
+@OPTION_INVENTORY
 @click.argument("host")
-@click.option("-i", "inventory", default="production")
-def hostvars(host, inventory):
+def hostvars(inventory, host):
+    """Shows all variables set for a host.
+
+    This means all gathered facts + all inventory variables (including those
+    from the vault).
+    """
     args = ["ansible", host, "-m", "debug", "-a",
             "var=hostvars[inventory_hostname]",
             f"-i{inventory}.inventory"]
@@ -69,11 +105,15 @@ def hostvars(host, inventory):
 
 
 @cli.command()
+@OPTION_INVENTORY
 @click.argument("glob")
-@click.option("-i", "inventory", default="production")
 @click.option("--diff", is_flag=True)
-def vars(glob, inventory, diff):
-    """Show variables matching a glob."""
+def vars(inventory, glob, diff):
+    """Show variables matching a GLOB.
+
+    When DIFF is true, show the diff between the production and staging
+    inventories for the given GLOB.
+    """
     template = (f"ansible-inventory -i{{}}.inventory --toml --list"
                 f"| awk '$1 ~ /{glob}/' | sort -u")
 
@@ -90,11 +130,30 @@ def vars(glob, inventory, diff):
 
 
 @cli.command(**PASS_ARGS)
-@click.option("-i", "inventory", default="production")
-@click.argument("playbook")
 @click.pass_context
+@OPTION_INVENTORY
+@click.argument("playbook")
 def run(ctx, inventory, playbook):
-    """Run a playbook."""
+    """Run a playbook. 
+
+    Unknown options are passed along to ansible.
+    
+    \b
+    Run the icosprod.yml playbook:
+      icos run foo
+
+    \b
+    Run prometheus/vmagent.yml:
+      icos run prom/vm
+
+    \b
+    Run prometheus/vmagent.yml but only the 'conf' tags
+      icos run prom/vm conf
+
+    \b
+    Run prometheus/vmagent.yml, conf tags, with diffs
+      icos run prom/vm conf -D
+    """
     inventory += ".inventory"
     if not os.path.exists(inventory):
         die(f"Cannot find {inventory} in {os.getcwd()}")
@@ -108,14 +167,3 @@ def run(ctx, inventory, playbook):
 
     print(*args)
     os.execvp(args[0], args)
-
-
-if __name__ == "__main__":
-    # Try to find out in which directory this script lives.
-    home = os.path.dirname(os.path.realpath(sys.argv[0]))
-
-    # Change to the directory where the playbooks lives. Ansible uses the
-    # current working directory as a starting point when locating its files.
-    os.chdir(home)
-
-    cli()
