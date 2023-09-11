f7c12c0d8677fb59757505215b8ed9b254eb2432
klarakristina <klarabroman@live.se>
2023-09-11 16:40:04 +0200

    [restheart-backup] small refactoring + copy script to server

--- devops/roles/icos.restheart/templates/restore.py
+++ devops/roles/icos.restheart/templates/restore.py
@@ -1,32 +1,27 @@
-#!/usr/bin/python{{ python3_version }}
+#!/usr/bin/python
 import click
 import os
 import subprocess
-
-
-def run_cmd(cmd, input = None):
-    if input:
-        return subprocess.run(cmd.split(), input=input, check=True, capture_output=True)
-    else:
-        return subprocess.run(cmd.split(), check=True, capture_output=True)
+from subprocess import check_output
+import os
 
 
 def get_latest_backup_date(host, location):
     os.environ['BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK'] = "y"
 
-    list_all_backups = f"borg list --short {host}:{location}"
-    all_backups = run_cmd(list_all_backups)
+    list_all_backups = f"borg list --short {host}:{location}".split()
+    all_backups = check_output(list_all_backups)
     
-    return run_cmd("tail -1", all_backups.stdout).stdout.strip().decode("utf-8")
+    return check_output(["tail", "-1"], input=all_backups).strip().decode("utf-8")
 
 
 def restore_latest_backup(host, location, latest_backup_date):
-    extract_backup = f"borg extract --stdout {host}:{location}::{latest_backup_date}"
-    exec_container = f"docker exec -i restheart_mongodb_1 mongorestore --archive --drop"
+    extract_backup = f"borg extract --stdout {host}:{location}::{latest_backup_date}".split()
+    exec_container = f"docker exec -i restheart_mongodb_1 mongorestore --archive --drop".split()
 
-    extracted_backup = run_cmd(extract_backup)
+    extracted_backup = check_output(extract_backup)
 
-    return subprocess.run(exec_container.split(), input=extracted_backup.stdout, check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) 
+    return check_output(exec_container, input=extracted_backup, stderr=subprocess.STDOUT) 
 
 
 @click.command('restore', help=f'Restore latest RestHeart backup')
@@ -40,12 +35,12 @@ def restore(host, location):
 
     print("Found latest backup to be from: ", latest_backup_date)
 
-    log_file = "roles/icos.restheart/restheart_restore_log.txt"
+    log_file = "/var/log/restheart_restore_log.txt" # variable instead?
 
     restore_output = restore_latest_backup(host, location, latest_backup_date)
 
     with open(log_file, 'w') as f:
-        f.write(restore_output.stdout.decode("utf-8"))
+        f.write(restore_output.decode("utf-8"))
 
     print(f"Backup restore done. Log can be found in {log_file}")
 
