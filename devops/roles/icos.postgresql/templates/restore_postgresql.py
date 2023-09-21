846f574a95a77aae09d92de41f2aadf3e32d2af0
André Bjärby <andre.bjarby@nateko.lu.se>
2023-09-21 07:36:22 +0200

    postgres refactor - use idiomatic terminology for borg
    The borg documentation states that: "Borg stores a set of files in an
    archive. A repository is a collection of archives. "
    
    So "borg list" doesn't list dates, it lists archives (whose names are dates!).

--- devops/roles/icos.postgresql/templates/restore_postgresql.py
+++ devops/roles/icos.postgresql/templates/restore_postgresql.py
@@ -5,7 +5,7 @@ from subprocess import check_output
 
 BBCLIENT = '{{ bbclient_home }}' #/bin/bbclient'
 
-def get_latest_backup_date(host, location):
+def get_last_archive(host, location):
     os.environ['BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK'] = "y"
     os.environ['BORG_RELOCATED_REPO_ACCESS_IS_OK'] = "y"
 
@@ -15,10 +15,11 @@ def get_latest_backup_date(host, location):
                         shell=1, text=1).strip()
 
 
-def restore_latest_backup(host, location, db, user, ignore_role_stmts, latest_backup_date):
+def restore_latest_backup(host, location, db, user,
+                          ignore_role_stmts, archive_name):
     exec_container = f"docker exec -i {db} psql -U {user}"
 
-    extracted_backup = check_output(f"{BBCLIENT} extract --stdout {host}:{location}::{latest_backup_date}", shell=1)
+    extracted_backup = check_output(f"{BBCLIENT} extract --stdout {host}:{location}::{archive_name}", shell=1)
 
     if ignore_role_stmts:
         remove_role_stmts = check_output("egrep -v '^(CREATE|ALTER) ROLE'", shell=1,
@@ -33,13 +34,13 @@ def restore_postgresql(host, location, db, user, ignore_role_stmts):
 
     print("Running restore task")
 
-    latest_backup_date = get_latest_backup_date(host, location)
+    last_archive = get_last_archive(host, location)
 
-    print("Found latest backup to be from: ", latest_backup_date)
+    print("Found last backup to be: ", last_archive)
     
     log_file = f"{db}_restore_log.txt"
 
-    restoration_output = restore_latest_backup(host, location, db, user, ignore_role_stmts, latest_backup_date)
+    restoration_output = restore_latest_backup(host, location, db, user, ignore_role_stmts, last_archive)
 
     print("Extracted and transferred latest backup in docker container")
 
