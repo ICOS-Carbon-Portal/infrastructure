d6629c94b2f0909e29e0e06db2bf5103b98d2d5a
André Bjärby <andre.bjarby@nateko.lu.se>
2023-09-21 10:27:39 +0200

    postgres refactor - restore from repo path
    Instead of just specifying the path to the bbclient (which is normally not
    present on development machines), also allow a repo path to be specified.
    
    Since bbclient is just a thin wrapper around borg(1) we can use the same
    function - renamed to borg_get_latest - to execute both commands.

--- devops/roles/icos.postgresql/files/icos-postgresql-util/icos_postgresql_util.py
+++ devops/roles/icos.postgresql/files/icos-postgresql-util/icos_postgresql_util.py
@@ -1,15 +1,14 @@
 import os
-from subprocess import check_output, check_call
+from subprocess import check_call, check_output
 
 import click
 
 
 # UTILS
-def bbclient_get_latest(bb):
+def borg_get_latest(borg):
     os.environ['BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK'] = "y"
     os.environ['BORG_RELOCATED_REPO_ACCESS_IS_OK'] = "y"
-    return check_output(f"{bb} list --short --last 1", shell=1, text=1).strip()
-
+    return check_output(f"{borg} list --short --last 1", shell=1, text=1).strip()
 
 
 # CLI
@@ -20,8 +19,9 @@ def cli():
 
 
 @cli.command()
-@click.option('--bb', required=True, help="Path to bbclient executable",
+@click.option('--bb', help="Path to bbclient executable",
               type=click.Path(executable=True))
+@click.option('--repo', help="The bbserver repo from which to retrieve backups")
 @click.option("--norole", is_flag=True,
               help='Filter out ROLE statements during restore')
 @click.option("--user", required=True, help="Postgres user")
@@ -30,18 +30,57 @@ def cli():
 @click.option("--stdout", is_flag=True, help="Dump to stdout. For testing.")
 @click.option('--overwrite', 'overwrite', is_flag=True,
               help="Actually run commands which OVERWRITES database")
-def restore(bb, norole, user, container, stdout, overwrite):
-    """Restore a postgresql backup from bbclient into a docker container.
+def restore(bb, repo, norole, user, container, stdout, overwrite):
+    """Restore a postgresql database from a borg backup.
 
-    By default it just displays the command to run, use --overwrite to execute.
+    \b
+    Restoring a database is done in phases, each with it's own options:
+      * choose borg backup source
+      * massage the sql statements
+      * destination of the restore
+      * debugging or overwrite
+    
+    \b
+    # Choosing borg backup source
+    The database can be retrieved using the path to a bbclient. This uses the
+    host and repo location hardcoded into the specific bbclient.
+      restore --bb /home/cpmeta/.bbclient/bin/bbclient
+
+    \b
+    Next option is to specify --repo. This relies on the borg binary being in
+    your $PATH and any ssh host alias being configured:
+      restore --repo fsicos2:~bbserver/repos/rdflog.repo
+
+    \b
+    # Massage the sql statements
+    The --norole option will remove any '(CREATE|ALTER) ROLE' statements
+
+    \b
+    # Destination of the restore
+    --stdout\t\tsimply print sql statements to stdout
+    --container NAME\tpipe sql to psql in the given container
+
+    \b
+    # Debugging or overwrite
+    By default, this command simply prints the command to be executed
+    If the --overwrite option is given, it executes the command (thus
+    overwriting the target database)
     """
-    if stdout == bool(container):
-        raise click.UsageError("Use use either --stdout or --container")
+    if bool(bb) == bool(repo):
+        raise click.UsageError("Use either --bb or --repo")
+    
+    if bool(stdout) == bool(container):
+        raise click.UsageError("Use either --stdout or --container")
 
-    archive = bbclient_get_latest(bb)
+    if repo:
+        borg = f"BORG_REPO={repo} borg"
+    if bb:
+        borg = bb
+
+    archive = borg_get_latest(borg)
 
     # Build a command, start by extracting the database to stdout.
-    cmd = f"{bb} extract --stdout ::{archive}"
+    cmd = f"{borg} extract --stdout ::{archive}"
 
     # If the user wants to strip ROLE ddl's, add a hack for that.
     if norole:
