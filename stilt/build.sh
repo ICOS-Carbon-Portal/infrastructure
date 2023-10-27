30f9769fb12374f75b977d10cd5f308dcc93f744
André Bjärby <andre.bjarby@nateko.lu.se>
2023-10-27 09:47:19 +0200

    stilt - update build.sh

--- stilt/build.sh
+++ stilt/build.sh
@@ -2,48 +2,43 @@
 # 1. Build the base stilt image.
 # 2. Build the custom stilt image.
 # 3. Save the custom stilt image as a tarball.
-# 4. Save a log file
 
 set -eux
 set -o pipefail
 
 TAG_BASE=${TAG_BASE:-stiltbase}
 TAG_CUSTOM=${TAG_CUSTOM:-stiltcustom}
-
-LOG=$(mktemp /tmp/build-stilt.XXX)
-chmod o-r "$LOG"
-
+   
 function vault {
-    val=$(cd .. && ansible-vault decrypt --output - vault.yml | 
-                 awk "\$1 ~ /^$1/ { print \$2 }")
+    val=$(cd ../devops && ansible-vault decrypt --output - \
+          group_vars/all/vault.yml | awk "\$1 ~ /^$1/ { print \$2 }")
     if [[ -z "$val" ]]; then
         1>&2 echo "Could not find value for $1"
         exit 1
     fi
+    echo "$val"
 }
 
 if [[ "${JENASVNUSER+set}" != set ]]; then
-    JENASVNUSER=$(vault vault_stilt_jena_user)
+    JENASVNUSER="$(vault vault_stilt_jena_user)"
 fi
 
 if [[ "${JENASVNPASSWORD+set}" != set ]]; then
-    JENASVNPASSWORD=$(vault vault_stilt_jena_password)
+    JENASVNPASSWORD="$(vault vault_stilt_jena_password)"
 fi
 
-ARGS=(--build-arg=JENASVNUSER=$JENASVNUSER
-      --build-arg=JENASVNPASSWORD=$JENASVNPASSWORD)
+ARGS=(--build-arg=JENASVNUSER=${JENASVNUSER}
+      --build-arg=JENASVNPASSWORD=${JENASVNPASSWORD})
 
-docker build --no-cache --pull --tag="$TAG_BASE" "${ARGS[@]}" base | tee "$LOG"
+docker build --pull --tag="$TAG_BASE" "${ARGS[@]}" base
 
 DOCKERFILE="$PWD/custom/Dockerfile.tmp"
 trap 'rm -f -- "$DOCKERFILE"' EXIT
 sed "s/^FROM.*/FROM $TAG_BASE/" custom/Dockerfile > "$DOCKERFILE"
 
-docker build --no-cache --tag="$TAG_CUSTOM" -f "$DOCKERFILE" custom | tee -a "$LOG"
+docker build --tag="$TAG_CUSTOM" -f "$DOCKERFILE" custom
 
 CUSTOM_ID=$(docker images | awk "\$1 ~ /^$TAG_CUSTOM\$/ { print \$3 }")
 OUTPUT_FN="$TAG_CUSTOM-$CUSTOM_ID.tgz"
 
-docker save "$TAG_CUSTOM" | gzip -c > "$OUTPUT_FN"
-echo "$OUTPUT_FN"
-echo "$LOG"
+echo "docker save $TAG_CUSTOM | gzip -c > $OUTPUT_FN"
