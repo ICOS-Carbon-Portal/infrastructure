2f67caf6510d0829447b4e5750c4647631cb99bc
André Bjärby <andre.bjarby@nateko.lu.se>
2024-02-08 14:57:40 +0100

    sync_csv_files - updates

--- devops/roles/icos.stiltweb/files/stilt-utils/stilt_utils/sync_csv_files.py
+++ devops/roles/icos.stiltweb/files/stilt-utils/stilt_utils/sync_csv_files.py
@@ -18,13 +18,11 @@
 # removed as well for the new csv files take effect.
 
 import collections
+import concurrent.futures
 import csv
-import glob
 import os
-from pathlib import Path
 import re
-import sys
-from concurrent import futures
+from pathlib import Path
 
 import click
 
@@ -44,11 +42,11 @@ MATCHING_CSV = re.compile(r"stiltresult(\d{4})x[^_]+_\d+.csv")
 
 # This object serves as something to return to the process executor.
 SyncResult = collections.namedtuple(
-    "SyncResult", ["csvdir", "nwritten", "nnoexist", "nskipped"]
+    "SyncResult", ["csvroot", "nwritten", "nnoexist", "nskipped"]
 )
 
 
-def remove_csv_cache_file(station, year):
+def remove_csv_cache_file(stationroot, station, year):
     """Remove the stiltweb csv cache file for station/year.
 
     Stiltweb keeps a csv cache file (consisting of the 365*(24/3) ==
@@ -57,13 +55,11 @@ def remove_csv_cache_file(station, year):
     file. Stiltweb will automatically rebuild the cache file. This
     also has the benefit of making our update atomic.
     """
-    cache_files = glob.glob(
-        os.path.join(STILTWEB_STATIONS, station, year, "cache*.txt")
-    )
+    files = stationroot.joinpath(station, year).glob("cache*.txt")
     # Check our assumptions about cache files before removing any.
-    assert len(cache_files) in (0, 1), cache_files
-    if len(cache_files) > 0:
-        os.unlink(cache_files[0])
+    assert len(files) in (0, 1), files
+    if len(files) > 0:
+        os.unlink(files[0])
 
 
 def open_stilt_csv(path):
@@ -76,32 +72,34 @@ def open_stilt_csv(path):
     return (header, reader)
 
 
-def calculate_slotdir_path(station, row):
+def calculate_slotdir_path(stationroot, station, row):
     """Calculate the correct stiltweb slot path for a csv row.
 
     The first field of each csv row is the date field, parse that and
     use it to index into stiltweb's directory tree.
 
-    >>> calculate_slotdir_path('HEI', ['17349.25', '69.28', '16.01', '5'])
-    PosixPath('/data/stiltweb/stations/HEI/2007/07/2007x07x02x06')
+    >>> calculate_slotdir_path(Path('/stations'), 'HEI', ['17349.25', 1, 2])
+    PosixPath('/stations/HEI/2007/07/2007x07x02x06')
     """
     date, slot = parse_date_id(row[0])
     if slot is None:
         return None
     slotname = "%sx%02dx%02dx%s" % (date.year, date.month, date.day, slot)
-    return STILTWEB_STATIONS.joinpath(
+    return stationroot.joinpath(
         station, str(date.year), f"{date.month:02}", slotname
     )
 
 
-def extract_csv_for_station(csvdir, station, newname):
+def extract_csv_for_station(csvdir, stationroot, newname, overwrite):
     """Parse the large csv files for a station and write the small csv files.
 
     This is the main worker function for each spawned process.
 
-    newname looks like "csv_XX3nZE3l0ODO9QA-T9gqI0GU" and contains the PID.
+    newname is either None (which means to overwrite/update current csv files)
+    or looks like "csv_XX3nZE3l0ODO9QA-T9gqI0GU" and contains the PID.
     """
     assert newname is None or newname.startswith("csv_")
+    station = csvdir.name
     nnoexist = 0
     nwritten = 0
     nskipped = 0
@@ -112,7 +110,7 @@ def extract_csv_for_station(csvdir, station, newname):
         header, reader = open_stilt_csv(f.path)
         nwritten_save = nwritten
         for row in reader:
-            slotdir = calculate_slotdir_path(station, row)
+            slotdir = calculate_slotdir_path(stationroot, station, row)
             if slotdir is None:
                 nskipped += 1
                 continue
@@ -120,31 +118,45 @@ def extract_csv_for_station(csvdir, station, newname):
                 nnoexist += 1
                 continue
             destcsv = slotdir / "csv"
-            if destcsv.exists():
-                if newname is not None:
-
-                    destcsv.rename(destcsv.with_name(newname))
-
-
+            if destcsv.exists() and newname is not None:
+                destcsv.rename(destcsv.with_name(newname))
             with open(destcsv, "w") as f:
                 writer = csv.writer(f, delimiter=" ")
                 writer.writerow(header)
                 writer.writerow(row)
-
         if nwritten > nwritten_save:
-            remove_csv_cache_file(station, m.group(1))
+            remove_csv_cache_file(stationroot, station, m.group(1))
     return SyncResult(csvdir, nwritten, nnoexist, nskipped)
 
 
+def sync_csv_files(csvroot, stationroot, rename, overwrite):
+    # enumerate the source csv dirs and make sure the target station exists
+    csvdirs = []
+    for csvdir in csvroot.glob('*'):
+        assert stationroot.joinpath(csvdir.name).exists()
+        csvdirs.append(csvdir)
+
+    # start a extract_csv_for_station process for each source csv dir
+    todos = {}
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for csvdir in csvdirs:
+            args = (csvdir, stationroot, rename, overwrite)
+            future = executor.submit(extract_csv_for_station, *args)
+            todos[future] = csvroot.name
+        for future in concurrent.futures.as_completed(todos):
+            result = future.result()
+            print(result)
+
+
 @click.command()
-@click.argument("cvsroot", type=CLICK_EXISTING_DIR)
+@click.argument("csvroot", type=CLICK_EXISTING_DIR)
 @click.argument("stationroot", type=CLICK_EXISTING_DIR)
 @click.option("--rename", help="Rename existing csv files to this")
 @click.option("--overwrite", is_flag=True, help="Remove existing csv files")
-def cli(cvsroot, stationroot, rename, overwrite):
+def cli(csvroot, stationroot, rename, overwrite):
     """Syncs new csv files into the stiltweb/stations directory tree.
 
-    CVSROOT is the directory where new (large) csv files are found.
+    CSVROOT is the directory where new (large) csv files are found.
 
     STATIONROOT is the stiltweb/stations directory into which csv files should
     be written
@@ -164,25 +176,11 @@ def cli(cvsroot, stationroot, rename, overwrite):
     Same as the previous example but instead of overwriting existing csv
     files, they will be renamed to csv_XX3nZE3l0ODO9QA-T9gqI0GU
     """
-    if rename is None and overwrite is False:
-        raise click.UsageError("Must specify one of --rename or --overwrite")
+    if rename is None:
+        if overwrite is False:
+            raise click.UsageError("Must specify one of --rename or --overwrite")
+    else:
+        if overwrite is True:
+            raise click.UsageError("Use either --rename or --overwrite")
 
-    if rename is not None and overwrite is True:
-        raise click.UsageError("Use either --rename or --overwrite")
-
-    print(rename, overwrite)
-    return
-    stations = []
-    for csvdir in os.scandir(sys.argv[1]):
-        if not csvdir.is_dir():
-            continue
-        assert os.path.exists(os.path.join(STILTWEB_STATIONS, csvdir.name))
-        stations.append(csvdir)
-    with futures.ProcessPoolExecutor() as executor:
-        todos = {}
-        for csvdir in stations:
-            future = executor.submit(extract_csv_for_station, csvdir.path, csvdir.name)
-            todos[future] = csvdir.name
-        for future in futures.as_completed(todos):
-            result = future.result()
-            print(result)
+    sync_csv_files(csvroot, stationroot, rename, overwrite)
