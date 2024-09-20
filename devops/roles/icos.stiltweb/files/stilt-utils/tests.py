0769b7aae6ba6a621cdc6b7985963d07b97915e5
André Bjärby <andre.bjarby@nateko.lu.se>
2024-09-20 11:07:10 +0200

    stiltweb - tests for sync-stiltweb-to-stilt

--- devops/roles/icos.stiltweb/files/stilt-utils/tests.py
+++ devops/roles/icos.stiltweb/files/stilt-utils/tests.py
@@ -1,4 +1,5 @@
 from stilt_utils import sync_csv_files
+from stilt_utils import sync_stiltweb_to_stilt
 
 
 def test_sync_csv_files_cli(cli_runner):
@@ -21,3 +22,24 @@ def test_sync_csv_files_cli(cli_runner):
 
     iserr(["--overwrite", "--rename", "foo", "/tmp", "/tmp"],
           "Use either --rename or --overwrite")
+
+
+def test_sync_stiltweb_to_stilt_cli_runner(cli_runner):
+    def iserr(args, err):
+        r = cli_runner.invoke(sync_stiltweb_to_stilt.cli, args)
+        assert r.exit_code != 0
+        output = r.output.strip()
+        # print(f"{output=}")
+        assert output.endswith(err)
+
+    iserr([], "Missing argument 'NEWROOT'.")
+
+    iserr(["src", "dst"], "'src' does not exist.")
+
+    iserr(["/tmp", "dst"],"'dst' does not exist.")
+
+    iserr(["/tmp", "/tmp"],
+          'Expected "/tmp/stations" to be a directory with station symlinks!')
+
+    iserr(["/proc", "/tmp"],
+          '/proc and /tmp needs to be on the same filesystem')
