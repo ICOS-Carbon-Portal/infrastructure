61e6d3736ffab112067053ddc2f9e4895da8ee4c
Sundström Valter <mail@valter.nu>
2024-12-16 15:18:11 +0100

    Use a snapshot

--- ci/scripts/test_dev_inventory.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-source py_env/bin/activate && cd /root/infrastructure/devops \
-  && ansible-playbook -i dev.inventory -t postgis_setup core.yml \
-  && ansible-playbook -i dev.inventory -t rdflog core.yml \
-  && ansible-playbook -i dev.inventory -t restheart_setup core.yml
