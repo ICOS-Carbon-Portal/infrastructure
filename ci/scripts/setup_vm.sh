dca8591ae1bacc2eed47025cff1b324291d5304a
Sundström Valter <mail@valter.nu>
2024-12-18 13:52:16 +0100

    Fix rdflog port

--- ci/scripts/setup_vm.sh
+++ ci/scripts/setup_vm.sh
@@ -1,5 +1,5 @@
 set -e
-apt update -y && apt upgrade -y && apt install -y docker-compose-v2 python3 python-is-python3 python3-venv
+apt update -y && apt upgrade -y && apt install -y docker-compose-v2 python3 python-is-python3 python3-venv postgresql-client
 
 # Add default ubuntu user to docker group, to allow running docker-compose
 gpasswd -a ubuntu docker
