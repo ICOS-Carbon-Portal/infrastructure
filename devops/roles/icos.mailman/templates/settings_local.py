095cf0f3859d1d37476e2bd1f111f7cfa89d21b2
Andrew Debevec <andrew.debevec@nateko.lu.se>
2025-01-16 17:03:29 +0100

    Fix mailing list archives

--- devops/roles/icos.mailman/templates/settings_local.py
+++ devops/roles/icos.mailman/templates/settings_local.py
@@ -17,3 +17,8 @@ MAILMAN_REST_API_PASS = "{{ mailman_rest_pass }}"
 # https://github.com/maxking/docker-mailman/tree/main/web#disable-social-auth
 # This will remove all "login with google" etc buttons the web page.
 MAILMAN_WEB_SOCIAL_AUTH = []
+
+# https://github.com/maxking/docker-mailman/issues/614#issuecomment-2339390853
+# This fixes archiver issues stemming from Docker using 172.19.199.1 for outbound traffic
+# (ip masquerading)
+MAILMAN_ARCHIVER_FROM = ('172.19.199.1', '172.19.199.2', '172.19.199.3', '172.19.199.4')
