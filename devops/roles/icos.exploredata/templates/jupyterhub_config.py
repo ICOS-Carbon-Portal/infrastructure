608e9a61a3679fb5e05f5c892ecfb7677ce84b3f
ZogopZ <zoiszogop@gmail.com>
2025-10-22 10:49:55 +0200

    new explore data - prepare ansible files

--- devops/roles/icos.exploredata/templates/jupyterhub_config.py
+++ devops/roles/icos.exploredata/templates/jupyterhub_config.py
@@ -1,25 +1,29 @@
+# Standard library imports
 import sys
 import os
 
-# CONFIGURATION OF THE HUB
+# Related third party imports
+from dockerspawner import DockerSpawner
 
-# The ip address for the Hub process to *bind* to.
-c.JupyterHub.hub_ip = '0.0.0.0'
-# c.JupyterHub.log_level = logging.DEBUG
 
-# The host name that notebooks will use to connect to the hub. In our case,
-# this is assigned to the hub image by docker-compose.
-c.JupyterHub.hub_connect_ip = 'hub'
+c = get_config()
 
+# JupyterHub configuration
+# Maximum number of concurrent servers that can be active at a time.
+c.JupyterHub.active_server_limit = {{ exploredata_max_notebooks }}
 # We handle authentication in the reverse proxy
 c.JupyterHub.authenticate_prometheus = False
-
-# Shuts down all user servers on logout
-c.JupyterHub.shutdown_on_logout = True
-
-# https://jupyterhub.readthedocs.io/en/stable/reference/templates.html
-c.JupyterHub.template_paths = ["/srv/jupyterhub/templates"]
-
+# The dummy authenticator allows any username with a hardcoded password.
+c.JupyterHub.authenticator_class = 'dummy'
+c.DummyAuthenticator.password = os.environ['PASSWORD']
+c.JupyterHub.hub_connect_ip = 'hub'
+c.JupyterHub.hub_ip = '0.0.0.0'
+# Interval (in seconds) at which to update last-activity timestamps.
+# This is set much more aggressively than in the default configuration so that
+# we quickly can shut down idle servers.
+c.JupyterHub.last_activity_interval = 300
+# Maximum concurrent named servers that can be created by a user.
+c.JupyterHub.named_server_limit_per_user = 1
 {% if exploredata_type == "prod" %}
 # https://github.com/jupyterhub/jupyterhub-idle-culler
 c.JupyterHub.services = [
@@ -33,60 +37,127 @@ c.JupyterHub.services = [
         ],
     }]
 {% endif %}
+# Shuts down all user servers on logout
+c.JupyterHub.shutdown_on_logout = True
+# https://jupyterhub.readthedocs.io/en/stable/reference/templates.html
+c.JupyterHub.template_paths = ['/srv/jupyterhub/templates']
 
-# AUTHENTICATION
+# DockerSpawner configuration
+c.DockerSpawner.allowed_images = [
+    'registry.icos-cp.eu/exploredata.test.icos-notebooks',
+    'registry.icos-cp.eu/exploredata.test.summer-school',
+    'registry.icos-cp.eu/exploredata.test.pylib-examples',
+]
+c.DockerSpawner.debug = True
+c.DockerSpawner.host_ip = '0.0.0.0'
+c.DockerSpawner.network_name = os.environ['NETWORK_NAME']
+c.DockerSpawner.notebook_dir = '/home/jovyan'
+c.DockerSpawner.read_only_volumes = {
+    '/data/stiltweb/stations' : '/data/stiltweb/stations',
+    '/data/stiltweb/slots'    : '/data/stiltweb/slots',
+    '/data'                   : '/data'
+}
+c.DockerSpawner.remove_containers = True
+c.DockerSpawner.use_internal_ip = True
 
-# The DummyAuthenticator allows any username along with a hardcoded password.
-c.JupyterHub.authenticator_class = 'jupyterhub.auth.DummyAuthenticator'
-c.DummyAuthenticator.password = os.environ["PASSWORD"]
-
+# Maximum number of bytes a single-user notebook server is allowed to use.
+c.Spawner.mem_limit = '2G'
+# Maximum number of cpu-cores a single-user notebook server is allowed to use.
+c.Spawner.cpu_limit = 1
 # This tells jupyterhub that "root" is an admin user. That's supposed to give
 # the root user an admin panel, which I didn't get to work (because of
 # DummyAuthenticator?). What it does enable however, is executing "jupyterhub
 # token" in the hub container.
 c.Authenticator.admin_users = set(['root'])
-
 # We then say that root cannot login over the web. Executing "jupyterhub token"
 # will still work.
 c.Authenticator.blacklist = ['root']
+#######################################################################
 
 
-# CONFIGURATION OF THE NOTEBOOK CONTAINERS
-# https://github.com/jupyterhub/dockerspawner/blob/master/dockerspawner/dockerspawner.py
+nbs = {
+    "icos_jupyter": [
+        "curve_fitting_obspack.ipynb",
+        "ecosystem_site_anomaly_visualization.ipynb",
+        "icos_atmObs_statistics.ipynb",
+        "radiocarbon.ipynb",
+        "station_characterization.ipynb",
+    ],
+    "pylib_examples": [
+        "ex1_data.ipynb",
+        "ex1a_atmo_data.ipynb",
+        "ex1b_eco_data.ipynb",
+        "ex1c_ocean_data.ipynb",
+        "ex2_station.ipynb",
+        "ex3_multisource.ipynb",
+        "ex4_collection.ipynb",
+        "ex5_sparql.ipynb",
+        "ex6a_STILT_find.ipynb",
+        "ex6b_STILT_footprint_animation.ipynb",
+        "ex7_ObsPackData.ipynb",
+        "how_to_authenticate.ipynb",
+    ]
+}
 
-c.JupyterHub.spawner_class = 'docker'
-c.DockerSpawner.image = os.environ["NOTEBOOK_IMAGE"]
-c.DockerSpawner.network_name = os.environ["NETWORK_NAME"]
-c.DockerSpawner.notebook_dir = '/home/jovyan'
-c.DockerSpawner.remove_containers = True
-c.DockerSpawner.debug = True
-c.DockerSpawner.read_only_volumes = {
-    '/data/stiltweb/stations' : "/data/stiltweb/stations",
-    '/data/stiltweb/slots'    : "/data/stiltweb/slots",
-    '/data'                   : '/data'
+image_map = {
+    'icos-notebooks:latest': os.environ['ICOS_NOTEBOOKS_IMAGE'],
+    'pylib-examples:latest': os.environ['PYLIB_EXAMPLES_IMAGE'],
+    'summer-school:latest': os.environ['SUMMER_SCHOOL_IMAGE'],
 }
 
 
-# RESTRICTIONS ON THE NOTEBOOKS
+class CustomDockerSpawner(DockerSpawner):
+    @staticmethod
+    def options_form(spawner):
+        template_path = '/srv/jupyterhub/templates/custom_options_form.html'
+        with open(template_path, 'r') as f:
+            return f.read()
 
-# Maximum number of concurrent named servers that can be created by a user at a
-# time.
-c.JupyterHub.named_server_limit_per_user = 1
+    @staticmethod
+    def options_from_form(form_data):
+        options = {}
+        if form_data:
+            image_from_form, notebook = form_data.get('env')[0].split('&nb=')
+            options['image'] = image_map[image_from_form]
+            if notebook in nbs["icos_jupyter"]:
+                options["notebook"] = f"/lab/tree/icos-jupyter-notebooks/{notebook}"
+            elif notebook in nbs["pylib_examples"]:
+                options["notebook"] = f"/lab/tree/{notebook}"
+            elif notebook == "ICOS_flasksampling_fossilfuel.ipynb":
+                options["notebook"] = (
+                    f"/lab/tree/project-jupyter-notebooks/RINGO-T1.3/{notebook}"
+                )
+            elif notebook == "city-characterization-tool":
+                options["notebook"] = (
+                    f"/lab/tree/project-jupyter-notebooks/city-characterization-tool/city_characteristic_analysis.ipynb"
+                )
+            elif notebook == "network-view-tool":
+                options["notebook"] = (
+                    f"/lab/tree/project-jupyter-notebooks/network-view-tool/network_view.ipynb"
+                )
+            elif notebook == "envrifair_winterschool":
+                options["notebook"] = (
+                    f"/lab/tree/project-jupyter-notebooks/envrifair-winterschool/map"
+                )
+            elif notebook == "otc_data_reduction_workshop":
+                options["notebook"] = (
+                    f"/lab/tree/project-jupyter-notebooks/otc-data-reduction-workshop"
+                )
+            elif notebook == "summer_school":
+                pass
+            else:
+                raise ValueError("Wrong or no image selected")
 
-# Maximum number of bytes a single-user notebook server is allowed to use.
-c.Spawner.mem_limit = '2G'
+        return options
 
-# Maximum number of cpu-cores a single-user notebook server is allowed to use.
-c.Spawner.cpu_limit = 1
+    async def start(self):
+        if "image" not in self.user_options:
+            raise ValueError(
+                "You must select an environment before starting the server."
+            )
+        self.image = self.user_options["image"]
+        if "notebook" in self.user_options.keys():
+            self.default_url = self.user_options["notebook"]
+        return await super().start()
 
-# Maximum number of concurrent servers that can be active at a time.
-c.JupyterHub.active_server_limit = {{ exploredata_max_notebooks }}
 
-# Interval (in seconds) at which to update last-activity timestamps.
-# This is set much more aggressively than in the default configuration so that
-# we quickly can shut down idle servers.
-c.JupyterHub.last_activity_interval = 300
-
-# The override configuration file doesn't have to exist.
-load_subconfig(os.path.join(os.path.dirname(__file__),
-                            'jupyterhub_config_override.py'))
