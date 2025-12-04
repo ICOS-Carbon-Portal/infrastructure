4c6db857ee73dcd94bc2563c94710992b4dc5540
robban-github <robban.grabowski@gmail.com>
2025-12-04 16:24:53 +0100

    [Zabbix] Remove old icos.zabbix_custom_checks role

--- devops/roles/icos.zabbix_custom_checks/files/fsicos2/check_docker_running.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-###############################################
-# check_docker_num.sh - Zabbix version
-# Returns number of running docker containers
-###############################################
-docker ps -q 2>/dev/null | wc -l
